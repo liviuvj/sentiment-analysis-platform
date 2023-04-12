a6119cacaca5535ce4359738466489bc25e4583f
Liviu <ljx1002@alu.ubu.es>
2023-04-12 21:35:20 +0200

    Documentado Sprint 4 (Closes #24)

--- prototypes/quickstart.sh
+++ prototypes/quickstart.sh
@@ -4,6 +4,9 @@
 #
 # Quickstart script for the project's prototype.
 
+# Activate environment
+source ../.venv/bin/activate
+
 # Clone repository and run platform
 cd extraction
 git clone --depth 1 https://github.com/airbytehq/airbyte.git
