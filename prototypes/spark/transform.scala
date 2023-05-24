3fcb0540c8dcaab0c1729e2dc45360978b57fb5a
Liviu <ljx1002@alu.ubu.es>
2023-05-24 23:14:52 +0200

    Mejorado procesamiento de los datos (Closes #41)

--- prototypes/spark/transform.scala
+++ prototypes/spark/transform.scala
@@ -3,14 +3,8 @@ import org.apache.spark.sql.functions.explode
 import com.mongodb.spark._
 
 // Extract raw data
-val rawDF = spark.read.format("mongo").load()
+val rawDF = spark.read.option("encoding", "ISO-8859-1").format("mongo").load()
 val rawDataDF = rawDF.select($"_airbyte_data.data.id", explode($"_airbyte_data.data") as "tweet_data")
-val rawMentionsDF = rawDF.select($"_airbyte_data.data.id", explode($"_airbyte_data.data.entities.mentions") as "mentions")
-val rawURLsDF = rawDF.select($"_airbyte_data.data.id", explode($"_airbyte_data.data.entities.urls") as "urls")
-val rawHashtagsDF = rawDF.select($"_airbyte_data.data.id", explode($"_airbyte_data.data.entities.hashtags") as "hashtags")
-val rawAnnotationsDF = rawDF.select($"_airbyte_data.data.id", explode($"_airbyte_data.data.entities.annotations") as "annotations")
-val rawContextAnnotationsDF = rawDF.select($"_airbyte_data.data.id", explode($"_airbyte_data.data.context_annotations") as "context_annotations")
-val rawPlacesDF = rawDF.select($"_airbyte_data.data.id", explode($"_airbyte_data.includes.places") as "place_data")
 val rawUsersDF = rawDF.select(explode($"_airbyte_data.includes.users") as "user_data")
 
 // Create tweets Dataframe
@@ -39,25 +33,58 @@ val usersDF = rawUsersDF.select(
     $"user_data.public_metrics.listed_count".alias("listed_count")
 )
 
-// Create place DataFrame
-val placesDF = rawPlacesDF.select(
-    $"id".alias("tweet_id"),
-    $"place_data.id".alias("country_id"),
-    $"place_data.country".alias("country"),
-    $"place_data.country_code".alias("country_code"),
-    $"place_data.full_name".alias("full_name"),
-    $"place_data.geo.bbox".alias("geo"),
-    $"place_data.place_type".alias("place_type")
-)
-
 // Create mentions DataFrame
-val mentionsDF = rawMentionsDF.select(
-    $"id".alias("tweet_id"),
-    $"mentions.username".alias("username")
-)
+val mentionsDF = rawDataDF.filter(
+    $"tweet_data.entities.mentions".isNotNull
+    ).select(
+        $"tweet_data.id".alias("tweet_id"),
+        explode($"tweet_data.entities.mentions").alias("mentions")
+    ).select($"tweet_id", $"mentions.*").
+    select($"tweet_id", $"id".alias("user_id"), $"username")
+
+// Create annotations DataFrame
+val annotationsDF = rawDataDF.select(
+    $"tweet_data.id".alias("tweet_id"),
+    explode($"tweet_data.entities.annotations").alias("annotations")
+    ).select($"tweet_id", $"annotations.*").
+    filter($"probability" > 0.9).
+    select($"tweet_id", $"type", $"normalized_text".alias("annotation"))
+
+// Create context annotations DataFrame
+val contextAnnotationsDF = rawDataDF.select(
+        $"tweet_data.id".alias("tweet_id"),
+        explode($"tweet_data.context_annotations").alias("context_annotations")
+    ).select($"tweet_id", $"context_annotations.*").
+    select($"tweet_id", $"domain.name".alias("domain"), $"entity.name".alias("entity"))
+
+// Create annotations DataFrame
+val urlsDF = rawDataDF.select(
+    $"tweet_data.id".alias("tweet_id"),
+    explode($"tweet_data.entities.urls").alias("urls")
+    ).select($"tweet_id", $"urls.*").
+    select($"tweet_id", $"display_url")
+
+// Create hashtags DataFrame
+val hashtagsDF = rawDataDF.select(
+    $"tweet_data.id".alias("tweet_id"),
+    explode($"tweet_data.entities.hashtags").alias("hashtags")
+    ).select($"tweet_id", $"hashtags.*").
+    select($"tweet_id", $"tag")
+
+// Clean DataFrames by dropping null values and duplicates
+val cleanTweetsDF = tweetsDF.dropDuplicates
+val cleanUsersDF = usersDF.dropDuplicates
+val cleanMentionsDF = mentionsDF.na.drop.dropDuplicates
+val cleanAnnotationsDF = annotationsDF.na.drop.dropDuplicates
+val cleanContextAnnotationsDF = contextAnnotationsDF.na.drop.dropDuplicates
+val cleanUrlsDF = urlsDF.dropDuplicates
+val cleanHashtagsDF = hashtagsDF.dropDuplicates
 
 // Save clean data back to MongoDB
-tweetsDF.write.format("mongo").mode("append").option("collection", "clean_tweets").save()
-usersDF.write.format("mongo").mode("append").option("collection", "clean_users").save()
-placesDF.write.format("mongo").mode("append").option("collection", "clean_places").save()
-mentionsDF.write.format("mongo").mode("append").option("collection", "clean_mentions").save()
+cleanTweetsDF.write.format("mongo").mode("append").option("collection", "clean_tweets").save()
+cleanUsersDF.write.format("mongo").mode("append").option("collection", "clean_users").save()
+cleanMentionsDF.write.format("mongo").mode("append").option("collection", "clean_mentions").save()
+cleanAnnotationsDF.write.format("mongo").mode("append").option("collection", "clean_annotations").save()
+cleanContextAnnotationsDF.write.format("mongo").mode("append").option("collection", "clean_context_annotations").save()
+cleanUrlsDF.write.format("mongo").mode("append").option("collection", "clean_urls").save()
+cleanHashtagsDF.write.format("mongo").mode("append").option("collection", "clean_hashtags").save()
