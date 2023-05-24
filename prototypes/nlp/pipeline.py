2f4985506b88199c3b02f0734fa149473195837f
Liviu <ljx1002@alu.ubu.es>
2023-05-24 23:17:33 +0200

    Mejora de las tareas NLP (Closes #42, Closes #43, Closes #44, Closes #45)

--- prototypes/nlp/pipeline.py
+++ prototypes/nlp/pipeline.py
@@ -4,27 +4,47 @@ import pymongo
 # Connecto to MongoDB
 client = pymongo.MongoClient("mongodb://mongoroot:mongopassword@mongo-db:27017/raw_tweets?authSource=admin")
 
-# Select database and collection
+# Select database and collections
 db = client["raw_twitter"]
-collection = db["clean_tweets"]
+collection_tweets = db["clean_tweets"]
+collection_users = db["clean_users"]
+collection_mentions = db["clean_mentions"]
+collection_annotations = db["clean_annotations"]
+collection_context_annotations = db["clean_context_annotations"]
+collection_urls = db["clean_urls"]
+collection_hashtags = db["clean_hashtags"]
 
 # Get data
-documents = collection.find()
+tweets = collection_tweets.find()
 
 # Get tweet texts
-tweet_texts = [doc for doc in documents if doc["language"] == "en"]
+tweets_english = [doc for doc in tweets if doc["language"] == "en"]
 
-# Create sentiment analysis pipelines
+# Create analysis pipelines
 sentiment_classifier = pipeline("sentiment-analysis", model="cardiffnlp/twitter-roberta-base-sentiment-latest")
 emotion_classifier = pipeline("sentiment-analysis", model="cardiffnlp/twitter-roberta-base-emotion")
 topic_classifier = pipeline("sentiment-analysis", model="cardiffnlp/tweet-topic-21-multi")
+ner_classifier = pipeline("sentiment-analysis", model="tner/twitter-roberta-base-dec2021-tweetner7-all")
 
-def pipe(x):
-    x["sentiment"] = sentiment_classifier(x["text"])[0]["label"]
-    x["emotion"] = emotion_classifier(x["text"])[0]["label"]
-    x["topic"] = topic_classifier(x["text"])[0]["label"]
-    return x
+# Create iterator for the data
+def data_iterator(data):
+    for d in data:
+        yield d["text"]
 
-for doc in tweet_texts:
-    print(pipe(doc))
-    break
+# Sentiment classification
+for id, sentiment in enumerate(sentiment_classifier(data_iterator(tweets_english))):
+    tweets_english[id]["sentiment"] = sentiment["label"]
+
+# Emotion classification
+for id, emotion in enumerate(emotion_classifier(data_iterator(tweets_english))):
+    tweets_english[id]["emotion"] = emotion["label"]
+
+# Topic classification
+for id, topic in enumerate(topic_classifier(data_iterator(tweets_english))):
+    tweets_english[id]["topic"] = topic["label"]
+
+# Topic classification
+for id, entity in enumerate(ner_classifier(data_iterator(tweets_english))):
+    tweets_english[id]["entity"] = entity["label"]
+
+# Export data to OLAP DB
